bd086927dbf16a62fad27f6038833f436dd186ec
dastex <daniil.grammatikopulo@gmail.com>
2024-10-05 21:47:43 +0300

    Refactored

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,59 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/header/Header";
-import Footer from "@/components/footer/Footer";
-import { ReactNode } from "react";
+import React from "react";
+import Navigation from "@/components/navigation/navigation";
+import { Poppins } from 'next/font/google';
+import RootProvider from "@/app/providers";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
 
 export const metadata: Metadata = {
-  title: "PourPal",
-  description:
-    "PourPal is your go-to online store for premium alcoholic beverages. Discover a wide selection of wines, spirits, and exclusive collections. With a user-friendly interface and fast delivery, shopping with us is convenient and enjoyable.",
-  keywords: [
-    "wine",
-    "spirits",
-    "alcohol",
-    "beverages",
-    "shop",
-    "store",
-    "online",
-    "delivery",
-    "premium",
-    "exclusive",
-    "alcoholic beverages",
-    "buy wine",
-    "premium spirits",
-    "online alcohol store",
-    "whiskey",
-    "vodka",
-    "gin",
-    "wine delivery"
-  ],
-  authors: [
-    { name: "PourPal", url: "https://www.youtube.com/watch?v=mHrtsCGR8yk" }
-  ]
+    title: "PourPal - Alcohol store",
+    description:
+        "PourPal is your go-to online store for premium alcoholic beverages. Discover a wide selection of wines, spirits, and exclusive collections. With a user-friendly interface and fast delivery, shopping with us is convenient and enjoyable.",
+    keywords: [
+        "wine",
+        "spirits",
+        "alcohol",
+        "beverages",
+        "shop",
+        "store",
+        "online",
+        "delivery",
+        "premium",
+        "exclusive",
+        "alcoholic beverages",
+        "buy wine",
+        "premium spirits",
+        "online alcohol store",
+        "whiskey",
+        "vodka",
+        "gin",
+        "wine delivery"
+    ],
+    authors: [
+        { name: "PourPal", url: "https://www.youtube.com/watch?v=mHrtsCGR8yk" }
+    ]
 };
 
-export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="en" data-theme="light">
-    <body className="bg-gray-200">
-    <Header />
-    {children}
-    <Footer />
-    </body>
+    <html lang="en" className={`${poppins.className}`}>
+      <body className="flex flex-col h-screen">
+      <RootProvider>
+        <Navigation />
+        <main className="w-full bg-gray-50 grow">{children}</main>
+        <footer className="text-center">Ragazzo sporco copyright</footer>
+      </RootProvider>
+      </body>
     </html>
   );
 }
--- src/utils/groupBy.ts
+++ src/utils/groupBy.ts
@@ -1,12 +1,12 @@
-export const groupBy = <T extends Record<string, any>>(array: T[], key: keyof T): Record<string, T[]> => {
-  return array.reduce((acc: Record<string, T[]>, item: T) => {
-    const groupKey = item[key] as string;
+export const groupBy = <T extends Record<string, unknown>>(array: T[], key: keyof T): Record<string, T[]> => {
+    return array.reduce((acc: Record<string, T[]>, item: T) => {
+        const groupKey = item[key] as string;
 
-    if (!acc[groupKey]) {
-      acc[groupKey] = [];
-    }
+        if (!acc[groupKey]) {
+            acc[groupKey] = [];
+        }
 
-    acc[groupKey].push(item);
-    return acc;
-  }, {});
+        acc[groupKey].push(item);
+        return acc;
+    }, {});
 };
